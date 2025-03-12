57b1c869a466bd6417048ce756fe220d6f55d2bf
Valentin Barbotin <valentinbarbotin@pm.me>
2025-03-12 14:15:58 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+import lib.exemple
+
+def main(args):
+    print('Hello World!')
+    print(args)
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ tests/test_exemple.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestExemple(unittest.TestCase):
+    def test_exemple(self):
+        self.assertEqual(1, 1)
